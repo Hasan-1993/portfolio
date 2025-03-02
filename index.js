4be7f79101ac27fb430b2bac63ad7c045771a7bc
Hasan Iftikhar <111067803+Hasan-1993@users.noreply.github.com>
2025-03-02 23:26:22 +0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function vis(n) {
-    console.log(n);
-    if (document.getElementById(n).style.display == 'inline'){
-        document.getElementById(n).style.display = 'none';
-    } else {
-        document.getElementById(n).style.display = 'inline';
-    }
-}
